3f1c7d69baadd3f07f804a2994eded970385d307
Daniel Fraga <dan.fraga33@gmail.com>
2023-09-29 10:42:39 +0100

    updates

--- components/db.js
+++ components/db.js
@@ -2,10 +2,13 @@ import mongoose from 'mongoose';
 
 export default async function connectDB() {
 	try {
-		await mongoose.connect(process.env.MONGODB_URI2, {
-			useNewUrlParser: true,
-			useUnifiedTopology: true,
-		});
+		await mongoose.connect(
+			'mongodb+srv://danfraga:macro123@cluster0.ja2qgj7.mongodb.net/stocks?retryWrites=true&w=majority',
+			{
+				useNewUrlParser: true,
+				useUnifiedTopology: true,
+			}
+		);
 		console.log('Connected to MongoDB');
 	} catch (error) {
 		console.error('Error connecting to MongoDB:', error);
