0e768de1eb7a04268eeff1dba037515021974926
Daniel Fraga <dan.fraga33@gmail.com>
2023-10-08 09:29:38 +0100

    new updates

--- pages/Portfolio/index.jsx
+++ pages/Portfolio/index.jsx
@@ -106,6 +106,39 @@ const Portfolio = ({ pageProps }) => {
 
 											<div className="flex md:justify-between  items-center text-sm md:text-md">
 												<p>{stock.performance.toFixed(2) + '%'}</p>
+
+												{/* <Menu
+													as="div"
+													className="relative inline-block text-left"
+												>
+													<div>
+														<Menu.Button className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 relative z-10">
+															<BsThreeDotsVertical
+																onClick={(e) => e.stopPropagation}
+															/>
+														</Menu.Button>
+													</div>
+
+													<Transition
+														as={Fragment}
+														enter="transition ease-out duration-100"
+														enterFrom="transform opacity-0 scale-95"
+														enterTo="transform opacity-100 scale-100"
+														leave="transition ease-in duration-75"
+														leaveFrom="transform opacity-100 scale-100"
+														leaveTo="transform opacity-0 scale-95"
+													>
+														<Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none z-20">
+															<div className="py-1">
+																<Menu.Item>
+																	{({ active }) => (
+																		
+																	)}
+																</Menu.Item>
+															</div>
+														</Menu.Items>
+													</Transition>
+												</Menu> */}
 											</div>
 										</li>
 									</Link>
